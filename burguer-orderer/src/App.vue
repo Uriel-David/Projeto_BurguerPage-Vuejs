7448cf9a19674e7c61d959020bae6c06aeb59066
Uriel-David <uriel.david.qaa@gmail.com>
2021-12-26 18:36:36 +0000

    Projeto de página web para pedidos de hamburguers usando Vue.js.

---
+++ burguer-orderer/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <NavBar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
+</template>
+<script>
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+export default {
+  data: function() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  },
+  components: {
+    NavBar,
+    Footer
+  }
+}
+</script>
+
+<style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .main-container {
+    margin: 50px;
+    min-height: 350px;
+  }
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
